7fc52538621a80019ae2766b9f1e5653b9aa4b3e
Tanvi Shah <tshah@vmware.com>
2014-05-30 16:21:41 +0530

    Change in JUnit VerifyURIUtils test case

--- src/test/java/org/opengis/cite/iso19139/util/VerifyURIUtils.java
+++ src/test/java/org/opengis/cite/iso19139/util/VerifyURIUtils.java
@@ -30,7 +30,7 @@ public class VerifyURIUtils {
     @Ignore
     @Test
     // comment out @Ignore to run test (requires network connection)
-    public void resolveHttpUriAsDocument() throws SAXException, IOException {
+    public void resolveHttpUriAsDocument() throws SAXException, IOException, Exception {
         URI uriRef = URI.create("http://www.w3schools.com/xml/note.xml");
         Document doc = URIUtils.parseURI(uriRef);
         Assert.assertNotNull(doc);
@@ -41,7 +41,7 @@ public class VerifyURIUtils {
     @Ignore
     @Test
     // comment out @Ignore to run test (requires network connection)
-    public void resolveHttpUriAsFile() throws SAXException, IOException {
+    public void resolveHttpUriAsFile() throws SAXException, IOException, Exception {
         URI uriRef = URI.create("http://www.w3schools.com/xml/note.xml");
         File file = URIUtils.dereferenceURI(uriRef);
         Assert.assertNotNull(file);
@@ -49,7 +49,7 @@ public class VerifyURIUtils {
     }
 
     @Test
-    public void resolveClasspathResource() throws SAXException, IOException,
+    public void resolveClasspathResource() throws SAXException, IOException, Exception,
             URISyntaxException {
         URL url = this.getClass().getResource("/atom-feed.xml");
         Document doc = URIUtils.parseURI(url.toURI());
@@ -59,7 +59,7 @@ public class VerifyURIUtils {
     }
 
     @Test
-    public void resolveFileRefWithXInclude() throws SAXException, IOException,
+    public void resolveFileRefWithXInclude() throws SAXException, IOException, Exception,
             URISyntaxException {
         File file = new File("src/test/resources/Alpha-xinclude.xml");
         Document doc = URIUtils.parseURI(file.toURI());
@@ -75,7 +75,7 @@ public class VerifyURIUtils {
 
     @Test(expected = IllegalArgumentException.class)
     public void resolveMissingClasspathResource() throws SAXException,
-            URISyntaxException, IOException {
+            URISyntaxException, IOException, Exception {
         URL url = this.getClass().getResource("/alpha.xml");
         URI uri = (null != url) ? url.toURI() : null;
         Document doc = URIUtils.parseURI(uri);
