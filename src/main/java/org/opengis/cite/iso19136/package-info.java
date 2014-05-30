965c9f37e74c52f1314445606759411df3349363
Tanvi Shah <tshah@vmware.com>
2014-05-30 14:55:28 +0530

    Revert "Commiting the beta test suite for ISO 19139. The beta test suite covers Conformance Level A.1 tests. Please create a folder 'iso1939' under scripts and then copy the ISO19139  script folder from targetto the folder TE_BASE/scripts/iso19139"
    This reverts commit 427f67195c27a8be592a7f5b0ebbf03d1885c3ac.

--- src/main/java/org/opengis/cite/iso19136/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * This executable test suite (ETS) validates GML application schemas or data in
- * accord with ISO 19136:2007. The root package includes supporting classes of
- * general utility such as the main controller, listeners, and reporters.
- *
- * <p>Subsidiary packages correspond to distinct test groups such as conformance
- * classes.</p>
- */
-package org.opengis.cite.iso19136;
--- src/main/java/org/opengis/cite/iso19136/util/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * This package includes miscellaneous utility classes to support testing.
- */
-package org.opengis.cite.iso19136.util;
-
